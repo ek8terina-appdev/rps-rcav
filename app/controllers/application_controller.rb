825844d033f056dfb5efff35e3c9a991822bb911
ek8terina <ek8terina@berkeley.edu>
2021-10-20 05:37:29 +0000

    adding paper scissors and home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,16 @@ class ApplicationController < ActionController::Base
     
     render({ :template => "game_templates/user_rock.html.erb"})
   end
+  def homepage
+    
+    render({ :template => "game_templates/rules.html.erb"})
+  end
+  def play_paper
+    
+    render({ :template => "game_templates/play_paper.html.erb"})
+  end
+  def play_scissors
+    
+    render({ :template => "game_templates/play_scissors.html.erb"})
+  end
 end
