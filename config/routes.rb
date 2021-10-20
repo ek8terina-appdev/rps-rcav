489f837ac82cda192525a5b7595db746c9128548
ek8terina <ek8terina@berkeley.edu>
2021-10-20 05:51:25 +0000

    full grade

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get("/rock", {:controller => "application", :action => "play_rock"})
-  get("/", {:controller => "application", :action => "homepage"})
-  get("/paper", {:controller => "application", :action => "play_paper"})
-  get("/scissors", {:controller => "application", :action => "play_scissors"})
+  get("/rock", {:controller => "application", :action => "play_rock", :layout =>"wrapper.html.erb"})
+  get("/", {:controller => "application", :action => "homepage", :layout =>"wrapper.html.erb"})
+  get("/paper", {:controller => "application", :action => "play_paper", :layout =>"wrapper.html.erb"})
+  get("/scissors", {:controller => "application", :action => "play_scissors", :layout =>"wrapper.html.erb"})
 
 end
